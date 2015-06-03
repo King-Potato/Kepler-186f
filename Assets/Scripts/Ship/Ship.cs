739671de286e81c9ddba6da7586805d4743fabc1
Meladath <rukadare@gmail.com>
2015-06-03 17:41:27 +0100

    Added ship health, score etc, and respawn function

--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -3,14 +3,35 @@
 public class Ship : MonoBehaviour
 {
   public IWeapon Weapon;
-
+  public float StartingHealth = 100.0f;
+  public float Health;
+  public ushort Score;
   public string FireInputName;
+  public Vector2 RespawnBoundaries;
+
+
+  void Start()
+  {
+    Health = StartingHealth;
+  }
 
   void Update()
   {
-    if(Input.GetButtonDown(FireInputName))
+    if (Health <= 0.0f)
+    {
+      Respawn();
+      return;
+    }
+
+    if (Input.GetButtonDown(FireInputName))
     {
       Weapon.Fire();
     }
+  }
+
+  void Respawn()
+  {
+    GetComponent<Transform>().position = new Vector3(Random.Range(RespawnBoundaries.x, RespawnBoundaries.y), Random.Range(RespawnBoundaries.x, RespawnBoundaries.y), 0.0f);
+    Health = StartingHealth;
   }
 }
