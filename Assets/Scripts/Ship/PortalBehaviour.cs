54cd51b4b0cf6970a736a5d310f0bca90a7327aa
Adam-Johnston <adam_j789@hotmail.co.uk>
2015-06-03 17:58:59 +0100

    Finished portal shit

--- Assets/Scripts/Ship/PortalBehaviour.cs
+++ Assets/Scripts/Ship/PortalBehaviour.cs
@@ -1,28 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PortalBehaviour : MonoBehaviour {
-    public Collider2D colliderObject;
+public class PortalBehaviour : MonoBehaviour
+{
+  public GameObject exitPoint;
+  private Transform exitTransform;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //if (transform.position.y > -50.0f)
-            transform.position.Set(transform.position.x, transform.position.y - 100.0f, transform.position.z);
-	}
+  // Use this for initialization
+  void Start()
+  {
+    exitTransform = exitPoint.GetComponent<Transform>();
+}
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        foreach (ContactPoint2D contact in collision.contacts)
-        {
-            //if (contact.otherCollider.GetComponent<Collider2D>() == colliderObject)
-            {
-                contact.otherCollider.GetComponent<Transform>().position.Set(0.0f, 0.0f, 0.0f);
-            }
-        }
-    }
+  void OnTriggerEnter2D(Collider2D collider)
+  {
+    //ParticleSystem particleSystem = collider.transform.FindChild("Thruster Particles").GetComponent<ParticleSystem>();
+    collider.GetComponent<Transform>().position = exitTransform.position;
+  }
 }
