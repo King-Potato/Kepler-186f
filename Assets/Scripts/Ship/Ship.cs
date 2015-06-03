45ce56cbfdc3278485bc0cf0c798f44b4468c687
Meladath <rukadare@gmail.com>
2015-06-03 19:15:27 +0100

    Modified respawn parameters to 2 vectors, can now specify x and y instead of just a square

--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -7,7 +7,8 @@ public class Ship : MonoBehaviour
   float m_health;
   ushort m_score;
   public string FireInputName;
-  public Vector2 RespawnBoundaries;
+  public Vector2 RespawnXBoundaries;
+  public Vector2 RespawnYBoundaries;
 
   void Start()
   {
@@ -30,11 +31,10 @@ public class Ship : MonoBehaviour
 
   void Respawn()
   {
-    GetComponent<Transform>().position = new Vector3(Random.Range(RespawnBoundaries.x, RespawnBoundaries.y), Random.Range(RespawnBoundaries.x, RespawnBoundaries.y), 0.0f);
+    GetComponent<Transform>().position = new Vector3(Random.Range(RespawnXBoundaries.x, RespawnXBoundaries.y), Random.Range(RespawnYBoundaries.x, RespawnYBoundaries.y), 0.0f);
     GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
     GetComponent<Rigidbody2D>().rotation = 0.0f;
     m_health = StartingHealth;
-
   }
 
   public void Kill()
