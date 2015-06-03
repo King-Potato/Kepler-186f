9cd20f5e69a1dafb41a7c36b323da3fadd7c2fab
Chris Latham <BackwardSpy@gmail.com>
2015-06-03 16:12:33 -0700

    Camera now moves to fit all four ships.

---
+++ Assets/Scripts/Camera/ZoomToFit.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ZoomToFit : MonoBehaviour
+{
+    public float MinDistance = 5.0f;
+
+    public float DistanceScale = 100.0f;
+
+    public List<Transform> Targets;
+    
+    void FixedUpdate()
+    {
+        // Get average position.
+        /*Vector3 center = Vector3.zero;
+        foreach (var t in Targets) center += t.position;
+        center /= Targets.Count;*/
+
+        // Get bounds containing all targets.
+        Rect bounds = new Rect();
+
+        foreach(var t in Targets)
+        {
+            bounds.xMax = Mathf.Max(t.position.x, bounds.xMax);
+            bounds.yMax = Mathf.Max(t.position.y, bounds.yMax);
+            bounds.xMin = Mathf.Min(t.position.x, bounds.xMin);
+            bounds.yMin = Mathf.Min(t.position.y, bounds.yMin);
+        }
+
+        // Get square diagonal size of bounds.
+        float size = Mathf.Sqrt(bounds.width * bounds.width + bounds.height * bounds.height);
+        size = Mathf.Sqrt(size / 100.0f);
+
+        float z = -size * DistanceScale;
+        if (z > -MinDistance) z = -MinDistance;
+
+        transform.position = new Vector3(bounds.center.x, bounds.center.y, -size * DistanceScale);
+    }
+}
