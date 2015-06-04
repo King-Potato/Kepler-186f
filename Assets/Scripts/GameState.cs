7f2aa9407176b3aefb6e22a2b3a37c4798759022
Chris Latham <BackwardSpy@gmail.com>
2015-06-04 13:05:36 -0700

    Added gamestate class

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,16 @@
+﻿public static class GameState
+{
+  public enum State
+  {
+    ReadyToPlay,
+    Running,
+    Results
+  }
+
+  public static State CurrentState;
+
+  static GameState()
+  {
+    CurrentState = State.ReadyToPlay;
+  }
+}
