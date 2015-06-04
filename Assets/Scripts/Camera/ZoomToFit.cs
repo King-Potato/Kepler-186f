d2d666a3e721817acbcf1c8bdf1a1503432fff69
Chris Latham <BackwardSpy@gmail.com>
2015-06-03 18:20:48 -0700

    Fixed camera tracking bug

--- Assets/Scripts/Camera/ZoomToFit.cs
+++ Assets/Scripts/Camera/ZoomToFit.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class ZoomToFit : MonoBehaviour
 {
@@ -13,15 +14,11 @@ public class ZoomToFit : MonoBehaviour
   {
     // Get bounds containing all targets.
     Rect bounds = new Rect();
-
-    foreach (var t in Targets)
-    {
-      bounds.xMax = Mathf.Max(t.position.x, bounds.xMax);
-      bounds.yMax = Mathf.Max(t.position.y, bounds.yMax);
-      bounds.xMin = Mathf.Min(t.position.x, bounds.xMin);
-      bounds.yMin = Mathf.Min(t.position.y, bounds.yMin);
-    }
-
+    bounds.xMin = Targets.Min(t => t.position.x);
+    bounds.yMin = Targets.Min(t => t.position.y);
+    bounds.xMax = Targets.Max(t => t.position.x);
+    bounds.yMax = Targets.Max(t => t.position.y);
+    
     // Get square diagonal size of bounds.
     float size = Mathf.Sqrt(bounds.width * bounds.width + bounds.height * bounds.height);
     size = Mathf.Sqrt(size / 100.0f);
@@ -30,5 +27,10 @@ public class ZoomToFit : MonoBehaviour
     if (z > -MinDistance) z = -MinDistance;
 
     transform.position = new Vector3(bounds.center.x, bounds.center.y, z);
+
+    Debug.DrawLine(new Vector3(bounds.xMin, bounds.yMin), new Vector3(bounds.xMin, bounds.yMax));
+    Debug.DrawLine(new Vector3(bounds.xMax, bounds.yMin), new Vector3(bounds.xMax, bounds.yMax));
+    Debug.DrawLine(new Vector3(bounds.xMin, bounds.yMin), new Vector3(bounds.xMax, bounds.yMin));
+    Debug.DrawLine(new Vector3(bounds.xMin, bounds.yMax), new Vector3(bounds.xMax, bounds.yMax));
   }
 }
