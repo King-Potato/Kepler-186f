be508fcff8a09b107256b87aa6e6dbecba2fdd15
Chris Latham <BackwardSpy@gmail.com>
2015-06-04 13:21:58 -0700

    Created UI controller

---
+++ Assets/UIControllerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIControllerScript : MonoBehaviour
+{
+  public Canvas ReadyToPlayUI;
+
+  public Canvas InGameUI;
+
+  public Canvas ResultsUI;
+
+  public void UpdateUIs()
+  {
+    switch (GameState.CurrentState)
+    {
+      case GameState.State.ReadyToPlay:
+        ReadyToPlayUI.enabled = true;
+        InGameUI.enabled = false;
+        ResultsUI.enabled = false;
+        break;
+
+      case GameState.State.Running:
+        ReadyToPlayUI.enabled = false;
+        InGameUI.enabled = true;
+        ResultsUI.enabled = false;
+        break;
+
+      case GameState.State.Results:
+        ReadyToPlayUI.enabled = false;
+        InGameUI.enabled = false;
+        ResultsUI.enabled = true;
+        break;
+    }
+  }
+}
