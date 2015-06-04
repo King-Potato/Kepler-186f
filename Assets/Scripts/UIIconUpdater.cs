2aaf303d6ca05193f943e8ca3a6432a0955761bc
Adam-Johnston <adam_j789@hotmail.co.uk>
2015-06-04 13:42:58 +0100

    Weapon icons

---
+++ Assets/Scripts/UIIconUpdater.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class UIIconUpdater : MonoBehaviour {
+	public Ship ship;
+	public static Dictionary<Ship, UIIconUpdater> instance = new Dictionary<Ship, UIIconUpdater>();
+
+	public void Start()
+	{
+		instance.Add(ship, this);
+	}
+
+	public static void SetImage(Ship ship, Sprite sprite)
+	{
+		UIIconUpdater instance = new UIIconUpdater();
+		if (UIIconUpdater.instance.TryGetValue(ship, out instance))
+		{
+			Image image = instance.transform.FindChild("Image").GetComponent<Image>();
+			if (image)
+				image.sprite = sprite;
+		}
+	}
+
+}
