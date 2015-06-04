afd6a258170496568a6ac0ba93bc9f9d3739b4b1
Adam-Johnston <adam_j789@hotmail.co.uk>
2015-06-04 10:52:40 +0100

    Portals, Asteroids, and General Awesomeness: A Study in Space Sciences

--- Assets/Scripts/Asteroid/AsteroidEmitter.cs
+++ Assets/Scripts/Asteroid/AsteroidEmitter.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class AsteroidEmitter : MonoBehaviour {
 	public List<Rigidbody2D> asteroids;
 	private static int poolSize;
-	private const int MAX_POOL_SIZE = 10;
+	public int asteroidCount = 10;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +14,13 @@ public class AsteroidEmitter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (poolSize < MAX_POOL_SIZE)
+		if (poolSize < asteroidCount)
 		{
 			Bounds bounds = transform.GetComponent<BoxCollider2D>().bounds;
 			Rigidbody2D asteroid = Instantiate<Rigidbody2D>(asteroids[Random.Range(0, asteroids.Count)]);
 			asteroid.transform.position = new Vector3(Random.Range(bounds.min.x, bounds.max.x), Random.Range(bounds.min.y, bounds.max.y), 0.0f);
+			asteroid.transform.localScale = new Vector3(Random.Range(3.5f, 5.0f), 1.0f, Random.Range(3.5f, 5.0f));
+			asteroid.GetComponent<CircleCollider2D> ().radius = Mathf.Sqrt(asteroid.transform.localScale.x * asteroid.transform.localScale.z / 3.141f) * 0.5f;
 			asteroid.velocity = new Vector3(Random.value * 2.0f - 1.0f, Random.value * 2.0f - 1.0f, 0.0f);
 			asteroid.angularVelocity = Random.value * 80.0f - 40.0f;
 			asteroid.angularDrag = 0.0f;
